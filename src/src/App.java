464fc8dc7b62c3f38b45bb12f197aa0a629081e4
Lukas <lukas.wais@outlook.de>
2020-11-24 21:11:32 +0100

    Update App.java

--- src/src/App.java
+++ src/src/App.java
@@ -1,115 +1,90 @@
-package src;
+package model;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-
 	private static int size = 5;
-	private static Employee[] employeeArr = new Employee[size];
+	private static Employee[] employees = new Employee[size];
 	private static int index = 0;
 
-	public static void main(String[] args) {
-		application();
-
-	}
-
 	private static int createID() {
-
-		return new Random().nextInt(1000);
-
+		// 123 is the default id
+		int id = new Random().nextInt(1000);
+		if (id == 123) {
+			id++;
+		}
+		return id;
 	}
 
 	private static void application() {
-
 		Scanner s = new Scanner(System.in);
 
-		Employee emp;
-		String empFirstName;
-		String empLasttName;
-
-		String again;
-
 		do {
+			System.out.println("Bitte legen Sie einen Mitarbeiter an: ");
+			System.out.println("Vorname: ");
+			String firstname = s.nextLine();
 
-			if (employeeArr.length >= size) {
-				doubleEmpArr();
-			}
+			System.out.println("Nachname: ");
+			String lastname = s.nextLine();
 
-			System.out.println("Bitte legen Sie einen Mitarbeiter an:");
-			System.out.print("Vorname ");
-			empFirstName = s.nextLine();
-
-			System.out.print("Nachname ");
-			empLasttName = s.nextLine();
-
-			Department department = new Department();
-			String dept;
-			System.out.println("In Welcher Abteilung soll " + empFirstName + " " + empLasttName + " arbeiten?");
+			System.out.println("In welcher Abteilung soll \t" + firstname + "\t" + lastname + "\t arbeiten?");
 			System.out.println(
-					"Für Development geben Sie d oder m für Management ein, oder eine beliebige andere Taste für eine Test-Abteilung: ");
-			dept = s.nextLine();
+					"Drücken Sie d für Development, m für Management, oder eine beliebige Taste für eine Test-Abteilung");
+			String dept = s.nextLine();
+			Department department;
+
 			if (dept.equalsIgnoreCase("d")) {
-				department = new Department("Development", 2, "Indien", "Bangaladesh");
+				department = new Department("Development", 1, "Indien", "Bangalore");
 			} else if (dept.equalsIgnoreCase("m")) {
-				department = new Department("Management", 1, "Deutschland", "Berlin");
+				department = new Department("Management", 2, "England", "London");
 			} else {
 				System.out.println("Eine Test-Abteilung wurde angelegt");
+				department = new Department();
 			}
 
-			emp = new Employee(empFirstName, empLasttName, createID(), department);
+			Employee employee = new Employee(firstname, lastname, createID(), department);
+			toArray(employee);
 
-			toArray(emp);
+			System.out.println("Sie haben folgenden Mitarbeiter angelegt: ");
+			employee.print();
 
-			System.out.println("Sie haben folgenden Mitarbeiter angelegt:");
-			System.out.println();
-			emp.print();
-
-			System.out.println("Möchten Sie noch einen Mitarbeiter anlegen?");
+			System.out.println("Möchten Sie einen weiteren Mitarbeiter anlegen?");
 			System.out.println("x für beenden. Beliebige Taste für weiter");
-			again = s.nextLine();
 
-			if (again.equals("x")) {
-
+			if (s.nextLine().equalsIgnoreCase("x")) {
 				System.out.println("Folgende Mitarbeiter wurden angelegt");
-
-				for (int j = 0; employeeArr[j] != null; j++) {
-
-					System.out.println(
-							employeeArr[j].id + " " + employeeArr[j].firstName + " " + employeeArr[j].lastName);
-
+				for (int i = 0; employees[i] != null; i++) {
+					System.out.println(employees[i].id + "\t" + employees[i].firstname + "\t" + employees[i].lastname);
 				}
-
-				System.out.println("Auf Wiedersehen!");
-
+				System.out.println("Auf Wiedersehen");
 				return;
 			}
 
 		} while (true);
-
 	}
 
-	private static Employee[] toArray(Employee e) {
-
-		employeeArr[index] = e;
-		index++;
-		size++;
-
-		return employeeArr;
-	}
-
-	private static Employee[] doubleEmpArr() {
-
-		Employee[] copy = employeeArr;
-		employeeArr = new Employee[size * 2];
-
-		int i = 0;
-		for (Employee employee : copy) {
-			employeeArr[i] = employee;
-			i++;
+	private static void toArray(Employee e) {
+		if (employees.length -1 >= index) {
+			doubleArraySize();
 		}
-
-		return employeeArr;
+		employees[index++] = e;
 	}
 
+	private static void doubleArraySize() {
+		Employee[] copy = employees;
+		// double the size
+		size *= 2; // size = size * 2
+		employees = new Employee[size];
+
+		// copy the employees
+		int i = 0;
+		for (Employee e : copy) {
+			employees[i++] = e;
+		}
+	}
+
+	public static void main(String[] args) {
+		application();
+	}
 }
